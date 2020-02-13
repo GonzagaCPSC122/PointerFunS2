828d8716446ff7e4ef941d936396ee6b11639fc1
Gina Sprint <sprint@gonzaga.edu>
2020-02-13 15:31:05 -0800

    Finish cmd line args

--- CmdLineArgsFun.cpp
+++ CmdLineArgsFun.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -12,6 +13,22 @@ int main(int argc, char * argv[]) {
 	
 	// write a for loop to iterate over each arg
 	// a C-string is a null terminated char array
+	// \0 is the null terminator
+	// example: seven
+	// 0 1 2 3 4 5
+	// s e v e n \0
+	// loop through the args and print them out as C++ strings
+	int i;
+	for (i = 0; i < argc; i++) {
+		string argString = argv[i]; // assign the C-string at index i to a C++ string
+		cout << argString << endl;
+	}
+	// #include <cstring> library with functions for working with C-strings
+	char myCString[] = "seven";
+	cout << strlen(myCString) << endl;
+	cout << sizeof(myCString) << endl;
+	// strcat() concatenate +
+	// strcmp() compare < <= == 
 
 	return 0;
 }
